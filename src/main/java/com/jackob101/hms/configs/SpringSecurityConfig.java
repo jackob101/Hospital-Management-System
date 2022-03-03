b46d0b1e9c637f6d4d2e1246e15ae49561522e7c
Jackob <pietrzyk.jakub001@gmail.com>
2022-03-03 15:51:28 +0100

    Figured out Auth0 role based access.

--- src/main/java/com/jackob101/hms/configs/SpringSecurityConfig.java
+++ src/main/java/com/jackob101/hms/configs/SpringSecurityConfig.java
@@ -1,34 +1,55 @@
 package com.jackob101.hms.configs;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Profile("!no-security")
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.audience}")
+    private String audience;
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
 
-    @Bean
+    private final OAuth2ResourceServerProperties resourceServerProps;
+
     JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        final var issuer = resourceServerProps.getJwt().getIssuerUri();
+        final var decoder = JwtDecoders.<NimbusJwtDecoder>fromIssuerLocation(issuer);
+        final var withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        final var tokenValidator = new DelegatingOAuth2TokenValidator<>(withIssuer, this::withAudience);
 
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
+        decoder.setJwtValidator(tokenValidator);
+        return decoder;
     }
+
+    private OAuth2TokenValidatorResult withAudience(final Jwt token) {
+        final var audienceError = new OAuth2Error(
+                OAuth2ErrorCodes.INVALID_TOKEN,
+                "The token was not issued for the given audience",
+                "https://datatracker.ietf.org/doc/html/rfc6750#section-3.1"
+        );
+
+        return token.getAudience().contains(audience)
+                ? OAuth2TokenValidatorResult.success()
+                : OAuth2TokenValidatorResult.failure(audienceError);
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,12 +58,27 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().sameOrigin();
 
         http.authorizeRequests()
+                .antMatchers("/specializations/**").hasAuthority("read:all")
+                .antMatchers("/user_details/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .csrf()
                 .ignoringAntMatchers("/h2-console/**")
                 .and()
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                .oauth2ResourceServer().jwt()
+                .decoder(jwtDecoder())
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+
+    }
+
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("permissions");
+        converter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return jwtConverter;
     }
 
 
